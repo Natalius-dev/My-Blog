7755290268522cca1bf19991208adc81614ba338
Ant Developers <55980467+Ant-Developers@users.noreply.github.com>
2021-02-28 21:26:11 +0700

    a

--- buttons.js
+++ buttons.js
@@ -1,6 +1,13 @@
-function goto(dir) {
+function a(dir) {
     window.location.href = window.location.href+dir;
 }
-function gotoWeb(dir) {
+function b(dir) {
     window.location.href = dir;
+}
+
+function goto(dir) {
+    setTimeout(a(dir), 250);
+}
+function gotoWeb(dir) {
+    setTimeout(b(dir), 250);
 }
